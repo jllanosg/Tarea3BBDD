98c2484ed16584887bb8c78888550559d3587557
Kirk Heim <kirk.heim@sansano.usm.cl>
2022-07-14 20:12:24 -0400

    CRUD DE TODO

--- Tablas.py
+++ Tablas.py
@@ -1,22 +1,50 @@
 
+from mailbox import NoSuchMailboxError
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Canciones(db.Model):
-    _tablename_ = 'canciones'
+    __tablename__ = 'canciones'
     id_cancion = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String,nullable=False)
     letra = db.Column(db.String,nullable=True)
     fecha_composicion = db.Column(db.Date,nullable=True)
 
-    classmethod
-    def create(cls,nombre,letra.fecha_composicion):
-        cancion = Canciones(nombre=nombre,letra=letra)
+    @classmethod
+    def create(cls,nombre,letra,fecha_composicion):
+        cancion = Canciones(nombre=nombre,letra=letra,fecha_composicion=fecha_composicion)
         return cancion.save()
 
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def update(self):
+        self.save()
+
+    #Hacemos uno con JSON pal postman
+    def json(self):
+        return{
+            'id_cancion': self.id_cancion,
+            'nombre': self.nombre,
+            'letra': self.letra,
+            'fecha_composicion': self.fecha_composicion,
+        }
 
 class Personas(db.Model):
-    _tablename_ = 'personas'
+    __tablename__ = 'personas'
     id_usuario = db.Column(db.Integer,primary_key=True)
     nombre = db.Column(db.String(100),nullable=False)
     apellido = db.Column(db.String(80),nullable=True)
@@ -27,15 +55,53 @@ class Personas(db.Model):
     artista_verificado = db.Column(db.Boolean)
     tipo_de_persona = db.Column(db.Boolean,nullable=False)
 
+    @classmethod
+    def create(cls,nombre,apellido,email,password,usuario_suscripcion_activa,artista_nombre_artistico,artista_verificado,tipo_de_persona):
+        persona = Personas(nombre=nombre,apellido=apellido,email=email,password=password,usuario_suscripcion_activa=usuario_suscripcion_activa,artista_nombre_artistico=artista_nombre_artistico,artista_verificado=artista_verificado,tipo_de_persona=tipo_de_persona)
+        return persona.save()
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def update(self):
+        self.save()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    #Hacemos uno con JSON pal postman
+    def json(self):
+        return{
+            'id_usuario': self.id_usuario,
+            'nombre': self.nombre,
+            'apellido': self.apellido,
+            'email': self.email,
+            'password': self.password,
+            'usuario_suscripcion_activa': self.usuario_suscripcion_activa,
+            'artista_nombre_artistico': self.artista_nombre_artistico,
+            'artista_verificado': self.artista_verificado,
+            'tipo_de_persona': self.tipo_de_persona,
+        }
+
 class Reproducciones(db.Model):
-    _tablename_='reproducciones'
+    __tablename__='reproducciones'
     id_cancion = db.Column(db.Integer,db.ForeignKey("canciones.id_cancion"),primary_key=True)
     id_usuario = db.Column(db.Integer,db.ForeignKey("personas.id_usuario"),primary_key=True)
     cantidad_reproducciones = db.Column(db.Integer,nullable=True)
     ultima_reproduccion = db.Column(db.DateTime,nullable=True)
 
 class Facturas(db.Model):
-    _tablename_ = 'facturas'
+    __tablename__ = 'facturas'
     id_factura = db.Column(db.Integer,primary_key=True)
     monto_facturado = db.Column(db.Integer)
     fecha_facturacion = db.Column(db.Date)
@@ -44,3 +110,41 @@ class Facturas(db.Model):
     metodo_de_pago = db.Column(db.String(100))
     fecha_hora_pago = db.Column(db.DateTime)
     id_usuario = db.Column(db.Integer,db.ForeignKey("personas.id_usuario"))
+    
+    @classmethod
+    def create(cls,monto_facturado,fecha_facturacion,fecha_vencimiento,estado,metodo_de_pago,fecha_hora_pago,id_usuario):
+        factura = Facturas(monto_facturado=monto_facturado,fecha_facturacion=fecha_facturacion,fecha_vencimiento=fecha_vencimiento,estado=estado,metodo_de_pago=metodo_de_pago,fecha_hora_pago=fecha_hora_pago,id_usuario=id_usuario)
+        return factura.save()
+        
+
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    def update(self):
+        self.save()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return self
+        except:
+            return False
+
+    #Hacemos uno con JSON pal postman
+    def json(self):
+        return{
+            'id_factura': self.id_factura,
+            'monto_facturado': self.monto_facturado,
+            'fecha_facturacion': self.fecha_facturacion,
+            'fecha_vencimiento': self.fecha_vencimiento,
+            'estado': self.estado,
+            'metodo_de_pago': self.metodo_de_pago,
+            'fecha_hora_pago': self.fecha_hora_pago,
+            'id_usuario': self.id_usuario,
+        }
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import flask
 from Config import config
-from Tablas import db
+from Tablas import*
 from flask_cors import CORS
+from flask import jsonify, request
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -19,13 +20,104 @@ def create_app(enviroment):
 
 enviroment = config["development"]
 app = create_app(enviroment)
-
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Mi primera API :P</h1>"
-
-
-
-
+#=====================================CANCIONES======================================#
+@app.route('/api/canciones', methods=['GET'])
+def canciones_select():
+    canciones = [cancion.json() for cancion in Canciones.query.all()]
+    response = jsonify(canciones)
+    return response
+#====================================================================================#
+@app.route('/api/canciones', methods=['POST'])
+def canciones_insert():
+    json = request.get_json()
+    canciones = Canciones.create(json['nombre'],json['letra'],json['fecha_composicion'])
+    response = jsonify(canciones.json())
+    return response
+#====================================================================================#
+@app.route('/api/canciones/<id_cancion>', methods=['DELETE'])
+def canciones_delete(id_cancion):
+    canciones = Canciones.query.filter_by(id_cancion=id_cancion).first()
+    canciones.delete()
+    return jsonify({'mensaje':'Cancion eliminada con exito'})
+#====================================================================================#
+@app.route('/api/canciones/<id_cancion>', methods=['PUT'])
+def canciones_put(id_cancion):
+    json = request.get_json()
+    canciones = Canciones.query.filter_by(id_cancion=id_cancion).first()
+    canciones.nombre = json['nombre']
+    canciones.letra = json['letra']
+    canciones.fecha_composicion = json['fecha_composicion']
+    canciones.update()
+    return jsonify(canciones.json())
+#====================================================================================#
+#=====================================PERSONAS=======================================#
+@app.route('/api/personas', methods=['GET'])
+def personas_select():
+    personas = [persona.json() for persona in Personas.query.all()]
+    response = jsonify(personas)
+    return response
+#====================================================================================#
+@app.route('/api/personas', methods=['POST'])
+def personas_insert():
+    json = request.get_json()
+    personas = Personas.create(json['nombre'],json['apellido'],json['email'],json['password'],json['usuario_suscripcion_activa'],json['artista_nombre_artistico'],json['artista_verificado'],json['tipo_de_persona'])
+    response = jsonify(personas.json())
+    return response
+#====================================================================================#
+@app.route('/api/personas/<id_usuario>', methods=['DELETE'])
+def personas_delete(id_usuario):
+    personas = Personas.query.filter_by(id_usuario=id_usuario).first()
+    personas.delete()
+    return jsonify({'mensaje':'Usuario eliminada con exito'})
+#====================================================================================#
+@app.route('/api/personas/<id_usuario>', methods=['PUT'])
+def personas_put(id_usuario):
+    json = request.get_json()
+    personas = Personas.query.filter_by(id_usuario=id_usuario).first()
+    personas.nombre = json['nombre']
+    personas.apellido = json['apellido']
+    personas.email = json['email']
+    personas.password = json['password']
+    personas.usuario_suscripcion_activa = json['usuario_suscripcion_activa']
+    personas.artista_nombre_artistico = json['artista_nombre_artistico']
+    personas.artista_verificado = json['artista_verificado']
+    personas.tipo_de_persona = json['tipo_de_persona']
+    personas.update()
+    return jsonify(personas.json())
+#====================================================================================#
+#=====================================FACTURAS=======================================#
+@app.route('/api/facturas', methods=['GET'])
+def facturas_select():
+    facturas = [factura.json() for factura in Facturas.query.all()]
+    response = jsonify(facturas)
+    return response
+#====================================================================================#
+@app.route('/api/facturas', methods=['POST'])
+def facturas_insert():
+    json = request.get_json()
+    facturas = Facturas.create(json['monto_facturado'],json['fecha_facturacion'],json['fecha_vencimiento'],json['estado'],json['metodo_de_pago'],json['fecha_hora_pago'],json['id_usuario'])
+    response = jsonify(facturas.json())
+    return response
+#====================================================================================#
+@app.route('/api/facturas/<id_factura>', methods=['DELETE'])
+def facturas_delete(id_factura):
+    factura = Facturas.query.filter_by(id_factura=id_factura).first()
+    factura.delete()
+    return jsonify({'mensaje':'Factura eliminada con exito'})
+#====================================================================================#
+@app.route('/api/facturas/<id_factura>', methods=['PUT'])
+def facturas_put(id_factura):
+    json = request.get_json()
+    facturas = Facturas.query.filter_by(id_factura=id_factura).first()
+    facturas.monto_facturado = json['monto_facturado']
+    facturas.fecha_facturacion = json['fecha_facturacion']
+    facturas.fecha_vencimiento = json['fecha_vencimiento']
+    facturas.estado = json['estado']
+    facturas.metodo_de_pago = json['metodo_de_pago']
+    facturas.fecha_hora_pago = json['fecha_hora_pago']
+    facturas.id_usuario = json['id_usuario']
+    facturas.update()
+    return jsonify(facturas.json())
+#====================================================================================#
 if __name__ == "__main__":
     app.run(debug=True)
